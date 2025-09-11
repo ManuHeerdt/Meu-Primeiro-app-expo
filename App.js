dd5b04534d397eb327fb7dc20839e340062c061d
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2025-09-11 12:39:40 +0000

    tyry

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Emanuelly Campos Heerdt</Text>
       <StatusBar style="auto" />
     </View>
   );
