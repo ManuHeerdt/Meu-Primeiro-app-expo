3d3c6ec6c90a0fd3bbeb94027b43d35d59bca2c8
Manu Heerdt <emanuelly.heerdt@escola.pr.gov.br>
2025-10-02 14:14:37 +0000

    hgjkuyk

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFonts, Montserrat_400Regular,Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+
+  const {fonteCarregada} = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserraBold": Montserrat_700Bold,
+  });
+  if(!fonteCarregada) {
+    return(<>
+    </>)
+  }
   return (
    <SafeAreaView>
      <StatusBar />
--- componentes/Topo.js
+++ componentes/Topo.js
@@ -2,16 +2,32 @@ import react from "react";
 import { Image,StyleSheet, Dimensions } from "react-native";
 
 import Texto from "./texto";
+import topo from "../../assets/topo.png";
+
+const widht = Dimensiond.get('screen').widht;
 
 export default function Topo({titulo}){
     return <>
+    <Image source={topo}/>
     <Texto style={estilos.titulo}> {titulo} </Texto>
     </>
 }
 
-const estilos = StyleSheet.create ({
-    topo{
+const estilos = StyleSheet.create({
+
+    topo:{
         width:"100%",
-        height: 578/
+        height: 578 / 786 * widht,
+    },
+    titulo:{
+        widht:"100",
+        position: "absolute",
+        textAlign:"center",
+        fontFamily:16,
+        lineHeight:26,
+        color:'withe',
+        fontWeight:"bold",
+        padding:16,
+
     }
 })
